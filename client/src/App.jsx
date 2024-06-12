bb6f619117b0eec09779790fbd087824120fd39f
vamshiparvatham <paravathamvamshi0105@gmail.com>
2024-06-12 12:24:07 +0530

    created header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,12 @@ import { Profile } from "./pages/Profile";
 import { Signin } from "./pages/Signin";
 import { Signup } from "./pages/Signup";
 import { About } from "./pages/About";
+import { Header } from "./components/Header";
 
 export default function App() {
   return(
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path = "/" element = {<Home/>}/>
         <Route path = "/about" element = {<About/>}/>
