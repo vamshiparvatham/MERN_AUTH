8925210e93947e336c3c7e18bf843ad6f6f7b8f2
vamshiparvatham <paravathamvamshi0105@gmail.com>
2024-06-12 21:27:23 +0530

    added test route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js'
 dotenv.config();
 const app = express();
 
@@ -12,4 +13,6 @@ mongoose.connect(process.env.MONGO).then(()=>{
 
 app.listen(3000,()=>{
     console.log("Server running");
-})
+});
+
+app.use('/api/user', userRoutes);
