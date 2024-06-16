332b67172f4ac645f97cd7dd26ffff83cf9ac7a3
vamshiparvatham <paravathamvamshi0105@gmail.com>
2024-06-16 21:52:53 +0530

    updated user api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import { test } from '../controllers/user.controller.js';
+import { verifytoken } from '../utils/Verifytoken.js';
+import { updateUser } from '../controllers/user.controller.js';
 const router = express.Router();
 
 router.get('/',test);
+router.post("/update/:id", verifytoken, updateUser);
 
 export default router;
---
+++ api/utils/Verifytoken.js
@@ -0,0 +1,13 @@
+import jwt from 'jsonwebtoken';
+import { errorHandler } from "../utils/error.js";
+
+export const verifytoken = (req,res,next)=>{
+    const token = req.cookies.access_token;
+    if(!token) return next(errorHandler(401,"Access Denied"))
+    jwt.verify(token, process.env.JWT_SECRET,(err,user)=>{
+        if(err) return next(errorHandler(403,"Invalid access token"));
+
+        req.user = user;
+        next();
+    })
+}
