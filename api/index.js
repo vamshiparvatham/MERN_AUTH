286cc6dabdd5d01f8783e6476f3c70b2e3164c48
vamshiparvatham <paravathamvamshi0105@gmail.com>
2024-06-18 12:06:51 +0530

    added pre production changes

--- api/index.js
+++ api/index.js
@@ -4,8 +4,17 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
 import cookieParser from 'cookie-parser';
+import path from 'path';
+
 dotenv.config();
 const app = express();
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname,'/client/dist')))
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client', 'dist', 'index.html'));
+})
 
 app.use(express.json());
 app.use(cookieParser());
